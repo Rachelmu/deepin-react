7b0fe777f3cd1b5e74d69a08ec9e40c2bbd267fd
JedenZhan <msygzzy1998@gmail.com>
2019-11-20 18:34:08 +0800

    add utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,19 +1,19 @@
 export const getDataByPath = (obj, url) => {
-  if (!obj) return obj;
+  if (!obj) return obj
   const keys = url.split('.'),
-    reg = /\[.+\]/;
-  let target = obj[keys.shift()];
+    reg = /\[.+\]/
+  let target = obj[keys.shift()]
   for (let i = 0, len = keys.length; i < len; i++) {
-    let currentKey = keys.shift();
+    let currentKey = keys.shift()
     if (currentKey.match(reg)) {
-      let arrIndex = parseInt(currentKey.match(reg)[0].slice(1, -1));
-      currentKey = currentKey.slice(0, currentKey.indexOf('['));
+      let arrIndex = parseInt(currentKey.match(reg)[0].slice(1, -1))
+      currentKey = currentKey.slice(0, currentKey.indexOf('['))
       target[currentKey] ? target = target[currentKey][arrIndex] : null
     } else if (target) {
-      target = target[currentKey];
+      target = target[currentKey]
     } else {
-      return target;
+      return target
     }
   }
-  return target;
+  return target
 }
