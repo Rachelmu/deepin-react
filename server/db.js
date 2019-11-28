997dd947aba8d7ba5e262408e359cdb4528c0b5c
JedenZhan <msygzzy1998@gmail.com>
2019-11-28 13:11:58 +0800

    新增连接池

--- server/db.js
+++ server/db.js
@@ -23,15 +23,21 @@ const opts = {
 // 创建连接池
 const connectPool = GenericPool.createPool(factory, opts)
 
-const curd = cb => {
+const curd = (dbName, cb) => {
+  if (typeof dbName === 'function') {
+    cb = dbName
+    dbName = 'demo'
+  }
   const databasePool = connectPool.acquire() // 获取一个连接, 这是一个 Promise
 
   databasePool
     .then(client => {
-      cb(client)
+      cb(client.db(dbName))
+    }, err => {
+      console.log('出错了', err)
     })
     .catch(err => {
-      throw err
+      console.log(err)
     })
 }
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -22,11 +22,13 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
   console.log(req.body) // 这里已经获取到了 body, 可以进行数据库操作了
-  db.collection('demo').find(req.body).toArray((err, result) => {
-    if (!err) {
-      console.log(result)
-      res.end(JSON.stringify(result))
-    }
+  curd(db => {
+    db.collection('demo').find(req.body).toArray((err, result) => {
+      if (!err) {
+        console.log(result)
+        res.end(JSON.stringify(result))
+      }
+    })
   })
 })
 
