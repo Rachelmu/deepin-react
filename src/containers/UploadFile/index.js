7aa1716f777f2bcd04518d13aeabb049dfe739bc
JedenZhan <msygzzy1998@gmail.com>
2020-01-24 21:46:01 +0800

    add

--- src/containers/UploadFile/index.js
+++ src/containers/UploadFile/index.js
@@ -15,17 +15,35 @@ class UploadFile extends Component {
 
   }
 
-  handleChange = () => {
-
+  handleChange = e => {
+    console.log('file changed', e.target.files)
+    const [file] = e.target.files
+    if (!file) return
+    this.setState({
+      file
+    })
   }
 
-  handleUpload = () => {
+  handleUpload = async () => {
+    console.log('uploading file')
+    if (!this.state.file) return
 
+    const fileChunkList = this.createFileChunk(this.state.file)
+    this.setState({
+      data: fileChunkList.map(({ file }, index) => {
+        return {
+          chunk: file,
+          hash: this.container.file.name + "-" + index
+        }
+      })
+    })
+    await this.uploadChunks()
   }
 
   createFileChunk = (file, length = sliceLength) => {
     const fileChunkList = [],
       chunkSize = Math.ceil(file.size / length)
+    console.log(file, chunkSize)
     let cur = 0
     while (cur < file.size) {
       fileChunkList.push({ file: file.slice(cur, cur + chunkSize) })
@@ -35,7 +53,20 @@ class UploadFile extends Component {
     return fileChunkList
   }
 
-  uploadChunks = async (chunks) => {
+  uploadChunks = async () => {
+    const requestList = this.state.data.map((chunk, hash) => {
+      const formData = new FormData()
+      formData.append('chunk', chunk)
+      formData.append('hash', hash)
+      formData.append('filename', this.state.file.name)
+      return { formData }
+    }).map(async ({ formData }) => {
+      request({
+        url: '3000',
+        data: formData
+      })
+    })
+    await Promise.all(requestList)
 
   }
 
