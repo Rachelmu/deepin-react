2bd2015a54b5eb11691a7186f96366265464a51d
JedenZhan <msygzzy1998@gmail.com>
2020-04-03 21:29:43 +0800

    reset

--- src/api/index.js
+++
@@ -1,7 +0,0 @@
-export const baseApi = 'http://localhost:3000'
-
-export const wsApi = 'ws://localhost:8888/testws'
-
-export const userApi = '/user'
-export const userRegister = userApi + '/register'
-export const userLogin = userApi + '/login'
--- src/containers/index.js
+++
@@ -1,9 +0,0 @@
-export const Home = () => require('./Home').default
-
-export const LifeCircle = () => require('./LifeCircle').default
-
-export const UploadFile = () => require('./UploadFile').default
-
-export const UploadPic = () => require('./UploadPic').default
-
-export const HooksExp = () => require('./Hooks').default
