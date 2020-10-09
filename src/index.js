49f5c3ff83478316a3c102a4323a89043374d045
zhangzhenyang <zhangzhenyang@aixuexi.com>
2020-10-09 20:04:58 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React, { Fragment } from 'react'
-import ReactDOM from 'react-dom'
+/*
+ * @Description:
+ * @Author: zhangzhenyang
+ * @Date: 2020-09-14 20:24:41
+ * @LastEditTime: 2020-09-14 20:58:03
+ * @LastEditors: zhangzhenyang
+ */
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
 
-import Step from '../module/Step'
+import Step from "../module/Step";
 
 const App = () => {
-  return (
-    <Fragment>
-      Hello World
-    </Fragment>
-  )
-}
+  return <Fragment>Hello World</Fragment>;
+};
 
-
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById("app"));
